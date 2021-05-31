6b83f6120131d1c1217b9722a9fa5844e20e9e4b
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-31 23:09:34 +0300

    1.1

---
+++ lesson1/task1/src/__tests__/Mailbox.test.jsx
@@ -0,0 +1,10 @@
+import { shallow } from "enzyme";
+import Mailbox from "../Mailbox";
+
+describe("Mailbox", () => {
+  it("should not render count if no unread messages", () => {
+    const wrappedComponent = shallow(<Mailbox />);
+
+    expect(wrappedComponent.find("mailbox__count").text()).toEqual(3);
+  });
+});
