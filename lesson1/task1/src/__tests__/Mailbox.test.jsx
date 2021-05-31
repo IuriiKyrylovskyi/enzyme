20dedd5a409a89a2a79e9cbe4dba9d93804b6833
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-31 23:37:47 +0300

    test2

--- lesson1/task1/src/__tests__/Mailbox.test.jsx
+++ lesson1/task1/src/__tests__/Mailbox.test.jsx
@@ -3,9 +3,16 @@ import { shallow } from "enzyme";
 import Mailbox from "../Mailbox";
 
 describe("Mailbox", () => {
+  it("should render unread messages", () => {
+    const unreadMessages = [1, 2, 3];
+    const wrappedComponent = shallow(<Mailbox unreadMessages={unreadMessages} />);
+
+    expect(wrappedComponent.find(".mailbox__count").text()).toEqual('3');
+  });
+
   it("should not render count if no unread messages", () => {
     const wrappedComponent = shallow(<Mailbox unreadMessages={[]} />);
 
-    expect(wrappedComponent.find("mailbox__count").exists()).toEqual(false);
+    expect(wrappedComponent.find(".mailbox__count").exists()).toEqual(false);
   });
 });
