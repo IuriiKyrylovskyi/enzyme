46b8377180eab7f33ae2770aa5dcc87c2cf65845
IuriiKyrylovskyi <iurii712@ukr.net>
2021-06-01 00:34:04 +0300

    1.3

--- lesson1/task3/src/__tests__/User.test.jsx
+++ lesson1/task3/src/__tests__/User.test.jsx
@@ -6,6 +6,12 @@ describe("User", () => {
   it("should render name", () => {
     const wrappedComponent = shallow(<User name={"Tom"} age={null} />);
 
-    expect(wrappedComponent.find(".user__name").text()).isEqual("Tom");
+    expect(wrappedComponent.find(".user__name").text()).toEqual("Tom");
+  });
+
+  it("should render age", () => {
+    const wrappedComponent = shallow(<User name={"Tom"} age={15} />);
+
+    expect(wrappedComponent.find(".user__age").text()).toEqual("15");
   });
 });
