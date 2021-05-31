a8f005f34f225d5af29b879c960d2ff653139540
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-31 23:26:22 +0300

    test

--- lesson1/task1/src/__tests__/Mailbox.test.jsx
+++ lesson1/task1/src/__tests__/Mailbox.test.jsx
@@ -4,8 +4,8 @@ import Mailbox from "../Mailbox";
 
 describe("Mailbox", () => {
   it("should not render count if no unread messages", () => {
-    const wrappedComponent = shallow(<Mailbox />);
+    const wrappedComponent = shallow(<Mailbox unreadMessages={[]} />);
 
-    expect(wrappedComponent.find("mailbox__count").text()).toEqual(3);
+    expect(wrappedComponent.find("mailbox__count").exists()).toEqual(false);
   });
 });
