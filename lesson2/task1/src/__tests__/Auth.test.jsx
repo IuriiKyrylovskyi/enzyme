53c19904276982d1090019991ce54b5498cac653
IuriiKyrylovskyi <iurii712@ukr.net>
2021-06-01 17:01:39 +0300

    prop("onLogin")()

---
+++ lesson2/task1/src/__tests__/Auth.test.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Auth from "../Auth";
+
+describe("Auth", () => {
+  it("should display a Login button if user is logged out", () => {
+    const wrappedComponent = shallow(<Auth />);
+
+    expect(wrappedComponent.find("Login")).toBeTruthy();
+  });
+
+  it("should display a Login button if user is logged out", () => {
+    const wrappedComponent = shallow(<Auth />);
+    wrappedComponent.find("Login").prop("onLogin")(); // emulate Login gets prop=onLogin & call() it to fullfill
+    wrappedComponent.find("Logout").prop("onLogout")();
+
+    expect(wrappedComponent.find("Login")).toBeTruthy();
+  });
+
+  it("should display a Logout button if user is logged in", () => {
+    const wrappedComponent = shallow(<Auth />);
+    wrappedComponent.find("Login").prop("onLogin")();
+
+    expect(wrappedComponent.find("Logout")).toBeTruthy();
+  });
+});
