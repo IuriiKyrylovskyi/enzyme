8eced9101706302500099bec3ec5483807d16b62
IuriiKyrylovskyi <iurii712@ukr.net>
2021-06-02 22:29:06 +0300

    Dialog

--- lesson2/task4/src/__tests__/Dialog.test.jsx
+++ lesson2/task4/src/__tests__/Dialog.test.jsx
@@ -21,10 +21,22 @@ describe("Dialog", () => {
   it("should be closed on icon click", () => {
     const mockOnClose = jest.fn();
     const wrappedComponent = mount(<Dialog isOpen onClose={mockOnClose} />);
-    // wrappedComponent.find(".dialog__close-btn").prop("onClose");
     wrappedComponent.find(".dialog__close-btn").simulate("click");
 
     expect(mockOnClose).toBeCalled();
-    // expect(wrappedComponent.find(".dialog").exists()).toBeFalsy();
+  });
+
+  it("should render title", () => {
+    const mockOnClose = jest.fn();
+    const wrappedComponent = shallow(<Dialog title={"Dialog"} isOpen onClose={() => mockOnClose()} />);
+
+    expect(wrappedComponent.find(".dialog__title").text()).toEqual("Dialog");
+  });
+
+  it("should render children", () => {
+    const mockOnClose = jest.fn();
+    const wrappedComponent = shallow(<Dialog children={"children"} isOpen onClose={() => mockOnClose()} />);
+
+    expect(wrappedComponent.find(".dialog__content").text()).toEqual("children");
   });
 });
